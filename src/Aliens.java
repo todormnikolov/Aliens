f957f1af1d4caf122fa799822715a1be0cd12cbc
Todor Nikolov <todor.nikolov@mail.bg>
2016-02-10 22:22:58 +0200

    resolved the problem with input and separating logic in more methods

--- src/Aliens.java
+++ src/Aliens.java
@@ -7,15 +7,7 @@ public class Aliens {
 
 		Scanner sc = new Scanner(System.in);
 
-		byte minTests = 1;
-		byte maxTests = 40;
-
-		byte numberOfTests;
-
-		do {
-			System.out.print("Input number of test: ");
-			numberOfTests = sc.nextByte();
-		} while (numberOfTests < minTests || numberOfTests > maxTests);
+		byte numberOfTests = getNumberOfTests(sc, "number of tests");
 
 		/**
 		 * store English words in columns with index 0, alien text - column
@@ -23,95 +15,32 @@ public class Aliens {
 		 */
 		String[][] data = new String[numberOfTests][2];
 
-		String input = "";
-
 		for (byte i = 0; i < numberOfTests; i++) {
 
 			System.out.println("Test " + (i + 1) + ":");
 
-			do {
-				/**
-				 * clear previous console input (only for manual input), comment
-				 * when use auto-generate English word
-				 */
-				sc.nextLine();
+			/**
+			 * comment 2 lines below if use auto-generation input
+			 */
+			data[i][0] = getWord(sc, "an English word");
+			data[i][1] = getText(sc, "an alien text", data[i][0].length());
 
-				/**
-				 * comment 2 lines below if make test with auto-generate an
-				 * English word
-				 */
-				System.out.print("Input an English word: ");
-				input = sc.next().toLowerCase();
-
-				/**
-				 * uncomment next line to generate an English word
-				 */
-				// input = autoGenerateInput(3);
-
-			} while (!correctInputCheck(input));
-
-			data[i][0] = input;
-
-			do {
-				/**
-				 * clear previous console input (only for manual input), comment
-				 * when use auto-generate alien text
-				 */
-				sc.nextLine();
-
-				/**
-				 * comment 2 lines below if make test with auto-generate alien
-				 * text
-				 */
-				System.out.print("Input an alien text: ");
-				input = sc.next().toLowerCase();
-
-				/**
-				 * uncomment next line to generate alien text
-				 */
-				// input = autoGenerateInput(90000);
-
-			} while (!correctInputCheck(input) || !compareStringLengths(data[i][0].length(), input.length()));
-
-			data[i][1] = input;
-		}
-
-		String[] indexes;
-		int lastIndex;
-
-		System.out.println("----------------------------------------------");
-		System.out.println("		RESULT FROM TESTS");
-		System.out.println("----------------------------------------------");
-
-		for (byte i = 0; i < numberOfTests; i++) {
-
-			indexes = searchMatches(data[i][0], data[i][1]);
-
-			System.out.printf("Test %d\nFound matches: ", i + 1);
-
-			if (indexes == null) {
-				System.out.println(0);
-				continue;
-			}
-
-			lastIndex = indexes.length;
-
-			System.out.println(lastIndex--);
-			System.out.print("Start indexes: ");
-
-			for (int j = 0; j < lastIndex; j++) {
-				System.out.print(indexes[j] + " ");
-			}
-
-			System.out.println(indexes[lastIndex]);
+			/**
+			 * uncomment 2 lines below for auto-generation input
+			 */
+			// data[i][0] = getAutoGenerateWord(3);
+			// data[i][1] = getAutoGenerateText(90000, data[i][0].length());
 
 		}
+
+		printTitle("Result from tests");
+		printResult(data, numberOfTests);
 
 		/**
 		 * for output long strings uncheck "Limit console output" in
 		 * Window->Preferences->Run/Debug->Console
 		 */
-		// printInput(data);
+		printInput(data);
 
 		sc.close();
 
@@ -119,14 +48,110 @@ public class Aliens {
 
 	/**
 	 * 
-	 * @param strOneLength
-	 * @param strTwoLength
-	 * @return true when strTwoLength is equal or bigger from strOneLength,
+	 * @param sc
+	 * @param message
+	 * @return Get integer from console, check min and max boundaries and return
+	 *         number of tests.
+	 */
+	public static byte getNumberOfTests(Scanner sc, String message) {
+
+		byte minTests = 1;
+		byte maxTests = 40;
+
+		int numberOfTests;
+
+		do {
+			System.out.printf("Input %s: ", message);
+			numberOfTests = sc.nextInt();
+		} while (numberOfTests < minTests || numberOfTests > maxTests);
+
+		return (byte) numberOfTests;
+	}
+
+	/**
+	 * 
+	 * @param sc
+	 * @param message
+	 * @return Get a string for word from console, when it pass checks return
+	 *         that string.
+	 */
+	public static String getWord(Scanner sc, String message) {
+
+		String input;
+
+		do {
+			System.out.printf("Input %s: ", message);
+			input = sc.next();
+		} while (!correctInputCheck(input));
+
+		return input.toLowerCase();
+	}
+
+	/**
+	 * 
+	 * @param sc
+	 * @param message
+	 * @param wordLength
+	 * @return Get a string for text from console, when it pass checks return
+	 *         that string.
+	 */
+	public static String getText(Scanner sc, String message, int wordLength) {
+
+		String text = getWord(sc, message);
+
+		while (!compareStringLengths(wordLength, text.length())) {
+			text = getText(sc, message, wordLength);
+		}
+
+		return text;
+	}
+
+	/**
+	 * 
+	 * @param maxLength
+	 * @return Returns auto-generate string for word with maximum possible
+	 *         length pass as parameter maxLength.
+	 */
+	public static String getAutoGenerateWord(int maxLength) {
+
+		String input;
+
+		do {
+			input = autoGenerateInput(maxLength);
+		} while (!correctInputCheck(input));
+
+		return input.toLowerCase();
+	}
+
+	/**
+	 * 
+	 * @param maxLength
+	 * @param wordLength
+	 * @return Returns auto-generate string for text with maximum possible
+	 *         length pass as parameter maxLength, after check that length with
+	 *         word's length.
+	 */
+	public static String getAutoGenerateText(int maxLength, int wordLength) {
+
+		String text = getAutoGenerateWord(maxLength);
+
+		while (!compareStringLengths(wordLength, text.length())) {
+			text = getAutoGenerateText(maxLength, wordLength);
+		}
+
+		return text;
+	}
+
+	/**
+	 * 
+	 * @param wordLength
+	 * @param textLength
+	 * @return true when textLength is equal or bigger from wordLength,
 	 *         otherwise print proper message on console and return false.
 	 */
-	public static boolean compareStringLengths(int strOneLength, int strTwoLength) {
+	public static boolean compareStringLengths(int wordLength, int textLength) {
 
-		if (strOneLength > strTwoLength) {
+		if (wordLength > textLength) {
 			System.out.println(
 					"Incorrect input data! The input for alian text must not be smaller from english word in same test variant. Try again...");
 			return false;
@@ -162,7 +187,7 @@ public class Aliens {
 
 			c = input.charAt(i);
 
-			if (c < 'a' || c > 'z') {
+			if (c < 65 || c > 122) {
 				System.out.println("Incorrect input data! The input must be contain only latin letters. Try again...");
 				return false;
 			}
@@ -192,15 +217,63 @@ public class Aliens {
 
 			if (index != -1) {
 				output.append(index++ + " ");
-			} else if (output.length() == 0) {
-				return null;
 			} else {
 				break;
 			}
 
 		}
 
+		if (output.length() == 0) {
+			return null;
+		}
+
 		return output.toString().split(" ");
+	}
+
+	/**
+	 * Print formatted string for section title
+	 * 
+	 * @param title
+	 */
+	public static void printTitle(String title) {
+		System.out.println("----------------------------------------------");
+		System.out.printf("		%s\n", title.toUpperCase());
+		System.out.println("----------------------------------------------");
+
+	}
+
+	/**
+	 * Print the result: number of matches and each start index
+	 * 
+	 * @param data
+	 * @param numberOfTests
+	 */
+	public static void printResult(String[][] data, byte numberOfTests) {
+
+		String[] indexes;
+
+		for (byte i = 0; i < numberOfTests; i++) {
+
+			indexes = searchMatches(data[i][0], data[i][1]);
+
+			System.out.printf("Test %d\nFound matches: ", i + 1);
+
+			if (indexes == null) {
+				System.out.println(0);
+				continue;
+			}
+
+			int lastIndex = indexes.length;
+
+			System.out.println(lastIndex--);
+			System.out.print("Start indexes: ");
+
+			for (int j = 0; j < lastIndex; j++) {
+				System.out.print(indexes[j] + " ");
+			}
+
+			System.out.println(indexes[lastIndex]);
+		}
 	}
 
 	/**
@@ -216,9 +289,7 @@ public class Aliens {
 		int len;
 		char[] array;
 
-		System.out.println("----------------------------------------------");
-		System.out.println("		INPUT DATA");
-		System.out.println("----------------------------------------------");
+		printTitle("Input data");
 
 		for (int i = 0; i < lengthRow; i++) {
 			for (int j = 0; j < lengthCol; j++) {
@@ -258,10 +329,6 @@ public class Aliens {
 	public static String autoGenerateInput(int maxLength) {
 
 		Random rand = new Random();
-		String letters = "abcdefghijklmnopqrstuvwxyz";
-
-		char[] chars = letters.toCharArray();
-		int charArrayLength = letters.length();
 
 		int index;
 
@@ -269,8 +336,8 @@ public class Aliens {
 		int stringLength = rand.nextInt(maxLength) + 1;
 
 		for (int i = 1; i < stringLength; i++) {
-			index = rand.nextInt(charArrayLength);
-			result.append(chars[index]);
+			index = rand.nextInt(26) + 97;
+			result.append((char) index);
 		}
 
 		return result.toString();
